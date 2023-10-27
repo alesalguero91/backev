78014f890a2eedd9f10f16726c95915a59d500ef
Alejandro <nestorale.salguero@outlook.es>
2023-10-27 02:25:55 -0300

    agregando persona

---
+++ src/main/java/com/example/backev/Service/PersonaService.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.backev.Service;
+
+import com.example.backev.Entity.Persona;
+import com.example.backev.Interface.IPersonaService;
+import com.example.backev.Repository.PersonaRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author salguero
+ */
+@Service
+public class PersonaService implements IPersonaService{
+    @Autowired PersonaRepository personaRepo;
+
+    @Override
+    public List<Persona> verPersonas() {
+        List<Persona>per= personaRepo.findAll();
+        return per;
+    }
+
+    @Override
+    public void crearPersona(Persona per) {
+        personaRepo.save(per);
+    }
+
+    @Override
+    public void borrarPersona(Long id) {
+        personaRepo.deleteById(id);
+    }
+
+    @Override
+    public Persona buscarPersona(Long id) {
+        Persona per = personaRepo.findById(id).orElse(null);
+        return per;
+    }
+    
+}
+
