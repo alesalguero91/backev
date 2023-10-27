b9706241453eba64f07dfc60eca7ebfbd8a1b7e4
Alejandro <nestorale.salguero@outlook.es>
2023-10-27 00:23:13 -0300

    build docker

---
+++ src/main/java/com/example/backev/BackevApplication.java
@@ -0,0 +1,13 @@
+package com.example.backev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backev/BackevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
